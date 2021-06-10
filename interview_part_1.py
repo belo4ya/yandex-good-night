7da9ccdba06cfeb786b913131ea9ea8c1d3f7b06
belo4ya <41exey.kov41ev@gmail.com>
2021-06-10 15:07:06 +0300

    немного оптимизаций решений

--- interview_part_1.py
+++ interview_part_1.py
@@ -1,81 +1,96 @@
-# SQL
-# таблица (продукт дата цена)
-# если цена изменилась заносится данные
-# актуальный срез цен на заданную дату
+import math
+
+# ***************************** 1 *****************************
+# -------------------------------------------------------------
+# В коде ниже допущены ошибки, нужно исправить их:
+#
+# def get_first_matching_object(predicate, objects=[]):
+#   matching_objects = (obj for obj in objects if predicate(object))
+#   if matching_objects:
+#     object = matching_objects[0]
+#     return object
+#   else:
+#     return None
 
 
 def get_first_matching_object(predicate, objects):
-    matching_objects = [obj for obj in objects if predicate(obj)]
-    if matching_objects:
-        return matching_objects[0]
+    matching_objects = (obj for obj in objects if predicate(obj))
+    try:
+        return next(matching_objects)
+    except StopIteration:
+        return  # на самом деле None - тоже не лучший вариант, лучше raise
 
 
-# 1) изменяемый
-# дефолтный
-# аргумент
+# 1) изменяемый дефолтный аргумент
 # 2) object -> obj
 # 3) matching_objects - генератор
 # 4) излишний None
 
-# ------------------------------------------------------------
-# 5
-# 5
-# 5
+
+# ***************************** 2 *****************************
+# -------------------------------------------------------------
+# Стек с константным поиском максимального элемента.
+# push, pop, max должны работать за O(1) по времени.
 
 
-class MaxStack(object):
-    """
-    Стек с константным поиском максимального элемента.
-
-    push, pop, max должны работать за O(1) по времени.
-    """
+class MaxStack:
 
     def __init__(self):
-        self._store = []
-        self._stack_for_max_values = [float('-inf')]
+        self._stack = []
+        self._max_values = []
 
     def push(self, elem):
-        self._store.append(elem)
+        self._stack.append(elem)
 
-        if elem >= self._stack_for_max_values[-1]:
-            self._stack_for_max_values.append(elem)
+        if elem >= self._peek_max():
+            self._max_values.append(elem)
 
     def pop(self):
-        elem = self._store.pop()
+        elem = self._stack.pop()
 
-        if elem == self._stack_for_max_values[-1]:
-            self._stack_for_max_values.pop()
+        if elem == self._peek_max():
+            self._max_values.pop()
 
     def max(self):
-        return self._stack_for_max_values[-1]
+        if self._stack:
+            return self._peek_max()
+
+    def _peek_max(self):  # дефолтное значение помогло бы отказаться от if
+        return self._max_values[-1] if self._max_values else -math.inf
 
 
-# -----------------------------------------------------------
-
-"""
-Дан список целых чисел, повторяющихся элементов в списке нет.
-Нужно преобразовать это множество в строку, 
-сворачивая соседние по числовому ряду числа в диапазоны.
-
-Примеры:
-- [1, 4, 5, 2, 3, 9, 8, 11, 0] => "0-5,8-9,11"  
-- [1, 4, 3, 2] => "1-4"
-- [1, 4] => "1,4"
-- [1, 4, 5, 2, 3, 9, 8, 0] => "0-5,8-9"
-- [0, 1, 2, 3, 5, 8, 10, 12] => "0-3, 5, 8, 10, 12"
-- [0, 2, 4, 5, 8] => "0, 2, 4-5, 8"
-"""
+# ***************************** 3 *****************************
+# -------------------------------------------------------------
+# Дан список целых чисел, повторяющихся элементов в списке нет.
+# Нужно преобразовать это множество в строку,
+# сворачивая соседние по числовому ряду числа в диапазоны.
+#
+# Примеры:
+# - [1, 4, 5, 2, 3, 9, 8, 11, 0] => "0-5,8-9,11"
+# - [1, 4, 3, 2] => "1-4"
+# - [1, 4] => "1,4"
+# - [1, 4, 5, 2, 3, 9, 8, 0] => "0-5,8-9"
+# - [0, 1, 2, 3, 5, 8, 10, 12] => "0-3, 5, 8, 10, 12"
+# - [0, 2, 4, 5, 8] => "0, 2, 4-5, 8"
 
 
-def pretty(start, end=None):
-    return str(start) + "-" + str(end) if end is not None else str(start)
+def render(start, end=None):
+    start = str(start)
+    if end:
+        return start + '-' + str(end)
+
+    return start
 
 
 def compress(int_array):
+    """
+    TODO: переделать
+    TODO: doctests
+    """
     if len(int_array) == 0:
         return ''
     if len(int_array) == 1:
-        return pretty(int_array[0])
+        return render(int_array[0])
 
     sorted_array = sorted(int_array)
     ranges = []
@@ -88,14 +103,31 @@ def compress(int_array):
 
         if current - prev > 1:
             if start_index == i - 1:
-                ranges.append(pretty(start))
+                ranges.append(render(start))
             else:
-                ranges.append(pretty(start, prev))
+                ranges.append(render(start, prev))
             start_index = i
 
     if start_index == len(sorted_array) - 1:
-        ranges.append(pretty(sorted_array[-1]))
+        ranges.append(render(sorted_array[-1]))
     else:
-        ranges.append(pretty(sorted_array[start_index], sorted_array[-1]))
+        ranges.append(render(sorted_array[start_index], sorted_array[-1]))
 
     return ','.join(ranges)
+
+
+# ***************************** 4 *****************************
+# -------------------------------------------------------------
+# SQL
+# таблица (продукт дата цена)
+# если цена изменилась заносится данные
+# актуальный срез цен на заданную дату
+stmt = """
+SELECT product_name, price FROM products p1
+WHERE updated_at = (
+    SELECT MAX(updated_at) FROM products p2
+    WHERE p1.product_name == p2.product_name AND
+          p2.updated_at <= :slice_date
+    GROUP BY p2.product_name
+    );
+"""
