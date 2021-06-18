b346d2c5437ad71469a75ab7741af5246cbe1fac
belo4ya <41exey.kov41ev@gmail.com>
2021-06-18 19:37:21 +0300

    Пишем осмысленные коммиты. Часть 09

--- interview_part_3.py
+++ interview_part_3.py
@@ -3,24 +3,34 @@
 # Даны две отсортированный последовательности.
 # Найти элементы из первой последовательности, которых нет во второй.
 
-# >>> minus([0, 1, 3, 3, 4, 6, 8, 8, 10], [1, 2, 4, 4, 8])
-# [0, 3, 3, 6, 10]
-
 
 def minus(it1, it2):
-    result = []
-    el2 = next(it2, None)
+    """
+    >>> minus(iter([0, 1, 3, 3, 4, 6, 8, 8, 10]), iter([1, 2, 4, 4, 8]))
+    [0, 3, 3, 6, 10]
 
+    >>> minus(iter([0, 1, 3, 5, 10]), iter([2, 4, 6]))
+    [0, 1, 3, 5, 10]
+
+    >>> minus(iter([3, 5, 5, 7]), iter([2, 3, 4, 6, 10]))
+    [5, 5, 7]
+
+    >>> minus(iter([1, 2, 5, 7]), iter([]))
+    [1, 2, 5, 7]
+
+    >>> minus(iter([1]), iter([1, 1, 1, 1, 2, 3, 4]))
+    []
+    """
+    result = []
+
+    el2 = next(it2, None)
     for el1 in it1:
 
-        while el2 is not None:
-
+        while el2 is not None and el1 != el2:
             if el1 < el2:
                 result.append(el1)
                 break
-            elif el1 == el2:
-                break
-                
+
             el2 = next(it2, None)
 
         if el2 is None:
