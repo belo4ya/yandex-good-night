a57a7dc69f517b65f17fdab4cd06e9f2d3ae6b7a
belo4ya <41exey.kov41ev@gmail.com>
2021-06-20 20:58:02 +0300

    Пишем осмысленные коммиты. Часть 09

--- sorts/quick_sort.py
+++
@@ -1,9 +0,0 @@
-def quick_sort(collection: list) -> list:
-    if len(collection) < 2:
-        return collection
-    pivot = collection.pop()
-    greater = []
-    lesser = []
-    for element in collection:
-        (greater if element > pivot else lesser).append(element)
-    return quick_sort(lesser) + [pivot] + quick_sort(greater)
