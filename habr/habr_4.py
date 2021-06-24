925efe8c0e5adb1dc92c3d63eb90bd1e10ba15e0
belo4ya <41exey.kov41ev@gmail.com>
2021-06-24 11:01:33 +0300

    Пишем осмысленные коммиты. Часть 13

--- habr/habr_4.py
+++ habr/habr_4.py
@@ -4,54 +4,6 @@
 # пример: [0, 0, 1, 1, 0, 1, 1, 0] -> 4
 
 
-# def max_ones_length(lst):
-#     max_ones_length = 0
-#
-#     # тут мы хотим получить сгруппированные 0 или 1 и их количество:
-#     subranges = []  # [(0, 2), (1, 2), (0, 1), (1, 2), (0, 1)]
-#     last_el = None  # последний элемент, который мы просмотрели
-#
-#     # идём по элементам списка
-#     for el in lst + [0]:  # [0] - это ручной триггер для обработки последнего элемента
-#
-#         if last_el != el:  # если произошла смена 0 на 1 или наоборот
-#             if el == 0:  # если это была смена 1 на 0
-#
-#                 # пример: subranges == [(0, 2), (1, 2), (0, 1), (1, 2)]
-#                 # у нас произошла смена 1 на 0, до смены единица шла 2 раза
-#                 # (см последний элемент subranges) - проверяем, вдруг это
-#                 # максимальная длина
-#                 try:
-#                     last_ones_length = subranges[-1][1]
-#                     max_ones_length = max(last_ones_length, max_ones_length)
-#                 except IndexError:
-#                     pass
-#
-#                 # а может если мы удалим один ноль между элементами 1 и 3,
-#                 # то получится более длинная последовательность единиц?
-#                 try:
-#                     gap_length = subranges[-2][1]
-#                     if gap_length == 1:
-#                         combined_ones_length = subranges[-1][1] + subranges[-3][1]
-#                         max_ones_length = max(combined_ones_length, max_ones_length)
-#                 except IndexError:
-#                     pass
-#
-#             # добавляем новый счётчик последовательности в subranges
-#             subranges.append((el, 1))
-#         else:
-#             # увеличиваем счётчик последней последовательности на 1
-#             subranges[-1] = (el, subranges[-1][1] + 1])
-#
-#             last_el = el
-#
-#             # костыль, граничное условие
-#             if len(subranges) == 2 and subranges[1][1] == 1:
-#                 return subranges[0][1] - 1
-#
-#     return max_ones_length
-
-
 def max_ones_length(lst):
     ones_len_current = 0
     ones_len_prev = 0
@@ -61,12 +13,16 @@ def max_ones_length(lst):
 
     prev_el = None
     for i, el in enumerate(lst):
+        print(f'{i}: max: {max_ones_len}')
+
+        if el == 1:
+            ones_len_current += 1
+        else:
+            zeros_len += 1
 
         if prev_el != el:  # произошла смена
-            print(ones_len_prev, zeros_len)
 
             if el == 0:  # с 1 на 0
-                max_ones_len = max(max_ones_len, ones_len_current + ones_len_prev)
 
                 if zeros_len == 1:
                     ones_len_prev = ones_len_current
@@ -77,15 +33,11 @@ def max_ones_length(lst):
             else:  # с 0 на 1
                 zeros_len = 0
 
-        if el == 1:
-            ones_len_current += 1
-        else:
-            zeros_len += 1
-
+        max_ones_len = max(max_ones_len, ones_len_current + ones_len_prev)
         prev_el = el
 
     return max_ones_len
 
 
-data = [0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1]
+data = [0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1]
 print(max_ones_length(data))
